6fc863908cb41bb4cc8482dc8ba8cf119f2b7e13
Đỗ Xuân  Tân <doxuantan1999a0@gmail.com>
2026-08-20 08:39:50 +0700

    day1-task2: cau hinh moi truong

---
+++ app/core/config.py
@@ -0,0 +1,16 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DATABASE_URL: str
+
+    SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    ALLOWED_ORIGINS: list[str] = []
+
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
+
+settings = Settings()
